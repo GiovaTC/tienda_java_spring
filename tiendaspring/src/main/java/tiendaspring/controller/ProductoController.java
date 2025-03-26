67223552253087737ea593d359fb3b85547a7e81
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-03-26 18:33:04 -0500

    Add files via upload

--- tiendaspring/src/main/java/tiendaspring/controller/ProductoController.java
+++ tiendaspring/src/main/java/tiendaspring/controller/ProductoController.java
@@ -1,5 +1,37 @@
 package controller;
 
-public class ProductoController {
+import tiendaspring.model.Producto;
+import tiendaspring.service.ProductoService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+	private final ProductoService productoService;
+	
+	public ProductoController(ProductoService productoService) {
+		this.productoService = productoService;
+	}
+	
+	@GetMapping
+	public List<Producto> obtenerTodos() {
+		return productoService.obtenerTodos();
+	}
+	
+	@PostMapping
+	public void agregarProducto(@RequestBody Producto producto) {
+		productoService.agregarProducto(producto);
+	}
+	
+	@PutMapping
+	public void actualizarProducto(@RequestBody Producto producto) {
+		productoService.actualizarProducto(producto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void eliminarProducto(@PathVariable int id) {
+		productoService.eliminarProducto(id);
+	}
 }
