fb7816ba7898c62323d87c496c0302dd5aba1d4d
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-03-26 17:17:44 -0500

    Add files via upload

--- tiendaspring/src/main/java/tiendaspring/repository/ProductoRepository.java
+++ tiendaspring/src/main/java/tiendaspring/repository/ProductoRepository.java
@@ -9,8 +9,48 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class ProductoRepository {
+	private final JdbcTemplate jdbcTemplate;
 	
+	public ProductoRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 	
+	public List<Producto> getAllProductos() {
+		String sql = "SELECT * FROM productos";
+		return jdbcTemplate.query(sql, new ProductoRowMapper());
+	}
+	
+	public void addProducto(Producto producto) {
+		String sql ) "INSERT INTO productos (nombre, tipo, fecha_fabricacion, fecha_vencimiento, imagen) VALUES (?,?,?,?,?)";
+		jdbcTemplate.update(sql, producto.getNombre(), producto.getTipo(), producto.getFechaFabricacion(), producto.getFechaVencimiento(), 
+		producto.getImagen());
+	}
+	
+	public void updateProducto(Producto producto) {
+		String sql = "UPDATE productos SET nombre=?, tipo=?, fecha_fabricacion=?, fecha_vencimiento=?, imagen=? WHERE id=?";
+		jdbcTemplate.update(sql, producto.getNombre(), producto.getTipo(), producto.getFechaFabricacion(), producto.getFechaVencimiento(),
+		producto.getImagen());
+	}
+	
+	public void deleteProducto(int id) {
+		String sql = "DELETE FROM productos WHERE id=?";
+		jdbcTemplate.update(sql, id);
+	}
+	
+	private static class ProductoRowMapper implements RowMapper<Producto> {
+		@Override
+		public Producto mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return new Producto(
+					rs.getInt("id"),
+					rs.getString("nombre"),
+					rs.getString("tipo"),
+					rs.getString("fecha_fabricacion"),
+					rs.getString("fecha_vencimiento"),
+					rs.getString("imagen")
+			);
+		}
+	}
 
 }
